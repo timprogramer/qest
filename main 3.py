2169f5e478615aa8991d2642213269d4d59307f2
Tim GF63 <tim@shatzir.com>
2022-01-19 19:36:28 +0900

    191

--- main 3.py
+++ main 3.py
@@ -5,8 +5,17 @@
 """
 
 import random
-qest=random.randint(1,10)
-answer=int(input("угадай число от 1 до 10"))
+
+qest=random.randint(1,100)
+answer=int(input("угадай число от 1 до 100"))
+
 while qest != answer:
-    answer = int(input("угадай число от 1 до 10"))
+    if answer<qest:
+        print("число больше")
+
+
+    elif answer > qest:
+        print("число меньше")
+
+    answer = int(input("угадай число от 1 до 100"))
 print("поздровляю ты выйграл 1 копейку")
